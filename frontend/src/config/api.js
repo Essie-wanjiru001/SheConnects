a292c1a53bcdaefe2d14aa598a1e97c04faa23fc
Essie-wanjiru001 <esthernjeriwanjiru287@gmail.com>
2025-03-09 20:01:23 +0300

    updates

--- frontend/src/config/api.js
+++ frontend/src/config/api.js
@@ -1,5 +1,5 @@
 const API_URL = process.env.NODE_ENV === 'production'
-  ? 'https://sheconnects_api.onrender.com'
+  ? 'https://sheconnects-api.onrender.com'
   : 'http://localhost:8000';
 
 export default API_URL;
